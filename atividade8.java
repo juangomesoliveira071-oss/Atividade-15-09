31fb9c74b29b1beb950e50420e87b7f577f6ff9b
juangomesoliveira071-oss <juangomesoliveira071@gmail.com>
2026-09-16 05:49:07 -0300

    Implement basic calculator operations in atividade8

--- atividade8.java
+++ atividade8.java
@@ -8,8 +8,26 @@ public class atividade8 {
         double n1 = entrada.nextDouble();
         System.out.print("Digite o segundo número: ");
         double n2 = entrada.nextDouble();
-        System.out.print("Qual a operação desejada: ");
-        double operação = entrada.nextDouble();
+        System.out.print("Qual a operação desejada:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n");
+        int op = entrada.nextInt();
+
+        double som = n1 + n2;
+        double sub = n1 - n2;
+        double mul = n1 * n2;
+        double div = n1 / n2;
+
+        if (op == 1) {
+            System.out.print("O resultado é: " + som);
+        }
+        else if (op == 2) {
+            System.out.print("O resultado é: " + sub);
+        }
+        else if (op == 3) {
+            System.out.print("O resultado é: " + mul);
+        }
+        else if (op == 4) {
+            System.out.print("O resultado é: " + div);
+        }
 
     }
 
